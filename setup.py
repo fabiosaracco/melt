360a41ee4e49f016fe0ffc3a1a4e46901fbe7ee4
sarawalk <f.sarawalk@gmail.com>
2023-05-10 18:10:30 +0200

    several bugs in the setup... :(

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='ameba_toolbox',
-    version='0.0.1',
+    name='melt',
+    version='0.0.2',
     author='Fabio',
     author_email='f.sarawalk@gmail.com',
     description='??? based on https://www.mdpi.com/2143956',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/fabiosaracco/ameba_toolbox',
+    url='https://github.com/fabiosaracco/melt',
     license='MIT',
-    packages=['ameba_toolbox'],
-    install_requires=['numpy,nltk,pandas,string,tqdm,bicm'],
+    packages=['melt'],
+    install_requires=['numpy','nltk','pandas','string','tqdm','bicm'],
 )
